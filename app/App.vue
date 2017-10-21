ba96ab1ae06bd6dcad69afa6d63ce58b7dbc305a
shelune <hieu@mios.fi>
2017-10-22 00:19:39 +0300

    more experiment with posting

--- app/App.vue
+++ app/App.vue
@@ -6,18 +6,24 @@
     <p v-else>But I'm a rich fucker with {{accountBalance}} ether!</p>
     <textarea v-model="message"></textarea>
     <button :disabled="!message" @click.prevent="postMessage">Post it!</button>
+    <hr>
+    <p v-if="messagePosted">{{ messagePosted }}</p>
+    <p>Transaction count: {{ txCount }}</p>
   </div>
 </template>
 <script>
 import sha from 'sha.js';
-import { default as Web3} from 'web3';
+import { default as Web3 } from 'web3';
 import { Talk } from './contracts';
 export default {
   data() {
     return {
+      account: null,
       error: null,
       accountBalance: null,
-      message: ""
+      message: "",
+      messagePosted: '',
+      txCount: 0,
     }
   },
   methods: {
@@ -26,20 +32,45 @@ export default {
         web3.eth.getAccounts(async (err,accounts) => err ? rej(err) : res(accounts[0]))
       );
     },
+    getTransactionById(id) {
+      return new Promise((res, rej) => {
+        return web3.eth.getTransaction(id, (err, tx) => {
+          return err ? rej(err) : res(tx)
+        })
+      });
+    },
+    getTransactionCount(id) {
+      return new Promise((res, rej) => {
+        return web3.eth.getTransactionCount(id, async (err, count) => {
+          console.log(`account ${id} has ${count} tx`)
+          return err ? rej(err) : res(count)
+        })
+      })
+    },
     async postMessage() {
       console.log("Posting message "+this.message);
-      var hash = sha("sha256").update(this.message).digest("hex");
+      //var hash = sha("sha256").update(this.message).digest("hex");
       var account = await this.getDefaultAccount();
       Talk.deployed()
-        .then(talk => { console.log("Deployed"); return talk.post(hash, { from: account }) })
-        .then(tx => console.log(tx))
-        .catch(err => this.error = err)
+        .then(talk => { console.log("Deployed"); return talk.post(this.message, { from: account }) })
+        .then(tx => {          
+          return this.getTransactionById(tx.tx)
+        })
+        .then(txData => {
+          this.messagePosted = web3.toAscii(txData.input)
+          //console.log('txData: ', web3.toAscii(txData.input))
+        })
+        .then(async () => {
+          this.txCount = await this.getTransactionCount(this.account)
+        })
+        .catch(err => {this.error = err; console.log('err: ', err)})
     }
   },
   async mounted() {
-    let account = await this.getDefaultAccount();
+    this.account =  await this.getDefaultAccount();
+    this.txCount = await this.getTransactionCount(this.account);
     let balance = await new Promise((res,rej) => 
-      web3.eth.getBalance(account,(err,balance) => err ? rej(err) : res(web3.fromWei(balance)))
+      web3.eth.getBalance(this.account,(err,balance) => err ? rej(err) : res(web3.fromWei(balance)))
     );
     this.accountBalance = parseFloat(balance);
   }
