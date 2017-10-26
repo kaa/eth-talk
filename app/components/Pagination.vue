180b83a3320f1ba4638e402659dc1b12802ce921
Joakim Beijar <jbeijar@mios.fi>
2017-10-26 14:17:23 +0300

    Fix pagination in edge cases

--- app/components/Pagination.vue
+++ app/components/Pagination.vue
@@ -1,13 +1,13 @@
 <template>
-  <nav>
+  <nav v-if="totalCount > 0">
     <ul class="pagination">
       <li v-if="currentPage == 1" class="disabled"><span aria-hidden="true">&laquo;</span></li>
       <li v-else><a href="#" v-on:click.prevent="$emit('change', currentPage-1)" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
-			<template v-for="ix in range(start,end)">
+			<template v-for="ix in range(start,end - 1)">
         <li v-if="ix==currentPage-1" class="active"><span>{{ix+1}}</span></li>
         <li v-else><a href="#" v-on:click.prevent="$emit('change', ix+1)">{{ix+1}}</a></li>
 			</template>
-      <li v-if="currentPage==pageCount" class="disabled"><span aria-hidden="true">&laquo;</span></li>
+      <li v-if="currentPage==pageCount" class="disabled"><span aria-hidden="true">&raquo;</span></li>
       <li v-else><a href="#" v-on:click.prevent="$emit('change', currentPage+1)" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>
     </ul>
   </nav>
